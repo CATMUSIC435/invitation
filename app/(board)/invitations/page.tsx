03fe9ce7d6a604e61ff65e40e69b5f8c768aa2be
dongiot <phuongdongiot@gmail.com>
2026-01-23 13:44:32 +0700

    fix: text

--- app/(board)/invitations/page.tsx
+++ app/(board)/invitations/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
         <main className="p-4">
             <div className="mt-10 mb-8 text-center">
                 <h1 className="flex items-center justify-center gap-4 mx-auto text-4xl font-bold mb-1 text-center bg-gradient-to-r from-[#844d15] to-[#844d15] bg-clip-text text-transparent" style={{ fontFamily: 'SVN Avo bold'}}>
-                    THƯ MỜI <br />MAGIC NIGHT <br /> POOL PARTY
+                    THƯ MỜI DXMD VIETNAM <br />MAGIC NIGHT <br /> POOL PARTY
                 </h1>
             </div>
             <InvitationCardSale />
