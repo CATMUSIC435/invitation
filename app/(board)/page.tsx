ce23263fa85bd2a7c53d41aa7e016512a6c1bdce
dongiot <phuongdongiot@gmail.com>
2026-01-23 10:49:26 +0700

    fix: change title

--- app/(board)/page.tsx
+++ app/(board)/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
         <main className="p-4">
             <div className="mt-10 mb-8 text-center">
                 <h1 className="flex items-center justify-center gap-4 mx-auto text-4xl font-bold mb-1 text-center bg-gradient-to-r from-[#844d15] to-[#844d15] bg-clip-text text-transparent" style={{ fontFamily: 'SVN Avo bold'}}>
-                    THƯ MỜI <br />DXMD VIETNAM AWARD 2026
+                    THƯ MỜI <br />DXMD VIETNAM AWARD 2025
                 </h1>
             </div>
             <InvitationCard />
