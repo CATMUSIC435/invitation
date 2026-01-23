48351e25fd685b54434c1e14af2204218410152f
dongiot <phuongdongiot@gmail.com>
2026-01-23 14:01:55 +0700

    update: text

--- app/(board)/invitations/page.tsx
+++ app/(board)/invitations/page.tsx
@@ -3,7 +3,7 @@ import InvitationCardSale from "@/components/atoms/invitation-card-sale";
 export default function Page() {
     return (
         <main className="p-4">
-            <div className="mt-10 mb-8 text-center">
+            <div className="mt-14 md:mt-10 mb-8 text-center">
                 <h1 className="flex items-center justify-center gap-4 mx-auto text-xl md:text-4xl font-bold mb-1 text-center bg-gradient-to-r from-[#844d15] to-[#844d15] bg-clip-text text-transparent" style={{ fontFamily: 'SVN Avo bold'}}>
                     THƯ MỜI DXMD VIETNAM <br />MAGIC NIGHT <br /> POOL PARTY
                 </h1>
