3f916598e73aaf5fbe667bb9725edcfe42b0a01e
phuongdongiot <catmusic435@gmail.com>
2026-05-18 10:43:09 +0700

    feat: add TopBranding component with logo link navigation

--- components/atoms/top-branding.tsx
+++ components/atoms/top-branding.tsx
@@ -9,9 +9,6 @@ export default function TopBranding({ url = '/' }: TopBrandingProps) {
     <div className="w-full flex flex-col items-center justify-center mb-1">
       <Link href={url} className="flex flex-col items-center justify-center hover:opacity-80 transition-opacity">
         <img src="/horizol-android-chrome-512x512.png" alt="DXMD Logo" className="w-36 md:w-44 object-contain mb-1" />
-        <h2 className="md:text-lg font-bold text-white uppercase tracking-widest text-center">
-          Công Ty Cổ Phần DXMD Việt Nam
-        </h2>
       </Link>
     </div>
   );
